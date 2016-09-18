8b3b153f7db6197449d64a9ae133697588b9c6c3
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-18 13:30:40 +0200

    Remove rspec

--- spec/parasut_rails_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe ParasutRails do
-  it "has a version number" do
-    expect(ParasutRails::VERSION).not_to be nil
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
-SimpleCov.start
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "parasut_rails"
