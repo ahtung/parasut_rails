f2e4be56ae9dd20a1bf8b6b4fe30fdc8fdbdb3fa
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-18 12:04:45 +0200

    Add coveralls gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
+SimpleCov.start
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "parasut_rails"
